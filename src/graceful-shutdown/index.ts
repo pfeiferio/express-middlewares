3735c707b9336e8bd8990cb95947e2b32799b808
Pascal Pfeifer <pascal@pfeifer.zone>
2026-02-28 14:56:28 +0100

    fix: export graceful-shutdown middleware and types

---
+++ src/graceful-shutdown/index.ts
@@ -0,0 +1,3 @@
+export {gracefulShutdownMiddleware} from "./gracefulShutdownMiddleware.js";
+export {createShutdownSignal} from "./utils/createShutdownSignal.js";
+export * from "./types/types.js";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./access-log/index.js";
 export * from "./body-parser/index.js";
+export * from "./graceful-shutdown/index.js";
